d017f9af03ae0da5a079545b4b8a2c2b0f4e29ab
georgeek <egormad@gmail.com>
2017-06-26 10:12:19 +0800

    refactoring code v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'It really works?';
-  name = "";
+  serverElements = [{type: 'server', name: 'PecheServer', content: 'FIR FIR!'}, {type: 'server', name: 'Егор Сервер', content: 'FIR FIR!'}];
+
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
